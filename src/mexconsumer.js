7dc96849aa2ae9d96d2f5f7f401aaa8f101b5cd6
nicolo-locascio <nicolo.locascio@consulenti.csi.it>
2025-01-17 11:40:12 +0100

    aggiornato componente per multitenancy

--- src/mexconsumer.js
+++ src/mexconsumer.js
@@ -38,7 +38,6 @@ function checkMex(mex) {
     if (!utility.checkNested(mex, "mex.body")) res.push("mex.body is mandatory");
 
     return res;
-
 }
 
 async function sendMex(body) {
@@ -48,7 +47,8 @@ async function sendMex(body) {
         bulk_id : body.payload.bulk_id,
         user_id : body.payload.user_id,
         tag : body.payload.tag,
-        correlation_id : body.payload.correlation_id
+        correlation_id : body.payload.correlation_id,
+        tenant : body.user.tenant ? body.user.tenant : conf.defaulttenant
     };
 
     eh.info("trying to save mex", JSON.stringify({
@@ -57,44 +57,40 @@ async function sendMex(body) {
     logger.debug("trying to save mex");
 
     var mex = body.payload;
-
-    mex.timestamp = utility.getDateFormatted(new Date(body.timestamp));
-    //mex.timestamp = utility.getDateFormatted(new Date()); per test
-
-    let user_id = mex.user_id.match(/^[a-fA-F0-9]{32}$/g)? mex.user_id:utility.hashMD5(mex.user_id);
-
+    mex.timestamp = new Date(body.timestamp).toISOString();
     mex.email = mex.email ? mex.email : {};
     mex.push = mex.push ? mex.push : {};
     mex.sms = mex.sms ? mex.sms : {};
     mex.mex = mex.mex ? mex.mex : {};
     mex.io = mex.io ? mex.io : {};
-    var insertSql = queryBuilder.insert().table("messages").values(
-        {
-            id: mex.id,
-            bulk_id: mex.bulk_id,
-            user_id: user_id,
-            email_to: mex.email.to,
-            email_subject: encrypt(mex.email.subject),
-            email_body: encrypt(mex.email.body),
-            email_template_id: mex.email.template_id,
-            sms_phone: mex.sms.phone,
-            sms_content: encrypt(mex.sms.content),
-            push_token: mex.push.token,
-            push_title: encrypt(mex.push.title),
-            push_body: encrypt(mex.push.body),
-            push_call_to_action: mex.push.call_to_action,
-            mex_title: encrypt(mex.mex.title),
-            mex_body: encrypt(mex.mex.body),
-            mex_call_to_action: mex.mex.call_to_action,
-            io: encrypt(JSON.stringify(mex.io)),
-            client_token: encrypt(JSON.stringify(body.user)),
-            sender: body.user.preference_service_name ? body.user.preference_service_name : body.user.client_name,
-            tag: mex.tag? mex.tag.split(",").map(e => e.trim()) : mex.tag,
-            correlation_id: mex.correlation_id,
-            timestamp: mex.timestamp,
-            memo: JSON.stringify(mex.memo)
-        }
-    ).sql;
+    let user_id = mex.user_id.match(/^[a-fA-F0-9]{32}$/g)? mex.user_id : utility.hashMD5(mex.user_id);
+
+    var insertSql = queryBuilder.insert().table("messages").values({
+        id: mex.id,
+        bulk_id: mex.bulk_id,
+        user_id: user_id,
+        email_to: mex.email.to,
+        email_subject: encrypt(mex.email.subject),
+        email_body: encrypt(mex.email.body),
+        email_template_id: mex.email.template_id,
+        sms_phone: mex.sms.phone,
+        sms_content: encrypt(mex.sms.content),
+        push_token: mex.push.token,
+        push_title: encrypt(mex.push.title),
+        push_body: encrypt(mex.push.body),
+        push_call_to_action: mex.push.call_to_action,
+        mex_title: encrypt(mex.mex.title),
+        mex_body: encrypt(mex.mex.body),
+        mex_call_to_action: mex.mex.call_to_action,
+        io: encrypt(JSON.stringify(mex.io)),
+        client_token: encrypt(JSON.stringify(body.user)),
+        sender: body.user.preference_service_name ? body.user.preference_service_name : body.user.client_name,
+        tag: mex.tag? mex.tag.split(",").map(e => e.trim()) : mex.tag,
+        correlation_id: mex.correlation_id,
+        timestamp: mex.timestamp,
+        memo: JSON.stringify(mex.memo),
+        tenant: body.user.tenant ? body.user.tenant : conf.defaulttenant
+    }).sql;
 
     await db.execute(insertSql);
 
@@ -105,27 +101,27 @@ async function sendMex(body) {
     logger.debug("mex saved");
 }
 
-logger.debug(JSON.stringify(process.env, null, 4));
-logger.debug(JSON.stringify(conf, null, 4));
+logger.info("environment:", JSON.stringify(process.env, null, 4));
+logger.info("configuration:", JSON.stringify(conf, null, 4));
 obj.consumer("mex", checkMex, null, sendMex, true)();
 
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
+function sleep(ms) {
+    return new Promise(resolve=> {
+        setTimeout(resolve, ms)
     })
 }
 
-async function shutdown(){
-    try{
+async function shutdown() {
+    try {
         await sleep(2000);
         logger.debug("STOPPING DATABASE");
         await db.end();
         logger.debug("STOPPED DATABASE");
-    }catch(e){
-        logger.error("error closing connection: ", e.message);
+    } catch(e) {
+        logger.error("error closing db connection: ", e.message);
         process.exit(1);
     }
 }
 
-process.on("SIGINT",shutdown);
-process.on("SIGTERM",shutdown);
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
